1f3e496628d072ec5ee58b85c01f023e15e3a95c
prushorth <maniprush3@gmail.com>
2026-01-05 23:17:34 -0800

    Upload java spring init

---
+++ src/main/java/com/cursorwave/cursor_wave/CursorWaveApplication.java
@@ -0,0 +1,13 @@
+package com.cursorwave.cursor_wave;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursorWaveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursorWaveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/cursorwave/cursor_wave/CursorWaveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cursorwave.cursor_wave;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CursorWaveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
